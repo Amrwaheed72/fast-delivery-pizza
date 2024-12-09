bec6664e465069b77ffca0e4a98dd18976f1600a
Amr Waheed <amrmohamed877@gmail.com>
2024-12-09 06:25:26 +0200

    fetching data of the menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,51 @@
-
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from './ui/Home'
+import Error from './ui/Error'
+import Menu, { loader as menuloader } from './features/menu/Menu'
+import Cart from './features/cart/Cart'
+import User from './features/user/CreateUser'
+import CreateOrder from './features/order/CreateOrder'
+import Order, { loader as orderLoader } from './features/order/Order'
+import AppLayout from "./ui/AppLayout"
+const route = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+        loader: menuloader,
+        errorElement: <Error />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />
+      },
+      {
+        path: '/user',
+        element: <User />
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
+    ]
+  }
+])
 function App() {
   return (
-    <div>
-      hello asdfasfasf
-    </div>
+    <RouterProvider router={route} ></RouterProvider>
   )
 }
 
