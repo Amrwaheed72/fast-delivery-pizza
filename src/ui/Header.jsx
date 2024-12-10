cffd74f2db02eb7624f4bcbbfaf5560e4b054a6f
Amr Waheed <amrmohamed877@gmail.com>
2024-12-10 17:33:07 +0200

    adding the styling with tailwind

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
 import Username from '../features/user/Username';
+import Logo from './Logo';
 
 function Header() {
   return (
     <header className="flex items-center justify-between border-b border-stone-400 bg-orange-800 px-4 py-3 uppercase text-stone-200 sm:px-6">
-      <Link className="tracking-widest" to="/">
+      <Link className="flex items-center gap-4 tracking-widest" to="/">
+        <Logo />
         Fast delivery Pizza
       </Link>
       <SearchOrder />
